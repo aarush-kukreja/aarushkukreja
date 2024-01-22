b9d100a708b9a990573553d6ae6ec29ddef12907
Aarush Kukreja <83042458+aarush-kukreja@users.noreply.github.com>
2024-01-22 16:51:14 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,24 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Aarush Kukreja',
+  description:
+    "Aarush Kukreja is a computer science student at Princeton University studying computer science and quant finance.",
+  keywords:
+    "Aarush Kukreja, Algorithmic Trading, Derivatives, Risk Management, Quantitative Analysis, Portfolio Optimization, Monte Carlo Simulation, Arbitrage, Volatility, Fixed Income, Financial Modeling, Time Series Analysis, Statistical Arbitrage, Asset Pricing, Market Microstructure, High-Frequency Trading, Econometrics, Black-Scholes Model, Value at Risk (VaR), Machine Learning in Finance, Behavioral Finance, Agile Methodology, Object-Oriented Programming, DevOps, Continuous Integration, Continuous Deployment, Scrum, Version Control, Microservices, Unit Testing, System Design, Debugging, Refactoring, API Development, Cloud Computing, Big Data, Data Structures, Algorithms, User Interface (UI) Design, User Experience (UX) Design, Software Development Lifecycle (SDLC), Database Management",
+  openGraph: {
+    locale: "en_US",
+    siteName: "Aarush Kukreja",
+    type: "website",
+    title: "Aarush Kukreja",
+    description:
+      "Aarush Kukreja is a computer science student at Princeton University studying computer science and quant finance.",
+    url: "https://aarushkukreja.com",
+    images: [
+      {
+        url: "./Card.png",
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
